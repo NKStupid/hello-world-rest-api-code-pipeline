b7b2967e43f6e111ec4170ed5c7ef57266cedcb5
EC2 Default User <ec2-user@ip-172-31-47-147.ec2.internal>
2021-04-13 20:41:17 +0000

    changed back

--- src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.amazon.ecs.springbootapp.springbootapp;
+package com.in28minutes.rest.webservices.restfulwebservices;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootappApplicationTests {
+public class RestfulWebServicesApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
